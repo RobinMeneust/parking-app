a3973b38838ff78bd2caf6e9ee72bc3bfb666f36
RobinMeneust <robin.meneust@hotmail.fr>
2023-04-12 22:55:50 +0200

    Date format is now checked for graphs

---
+++ UserGraphs.js
@@ -0,0 +1,130 @@
+let months=["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"];
+let year = "2023";
+let button = null;
+const regex = new RegExp('^([0-9]{4})$'); // used to check if the given date (year) is correct
+
+function initialize() {
+    button = document.getElementById('refreshDate');
+    button.addEventListener("click",function(){refreshDate();});
+
+    getExpensesValues().then((result) =>{
+        let expensesValues = result;
+        createExpensesGraph(expensesValues);
+    });
+    
+    getVisitsValues().then((result) =>{
+        let visitsValues = result;
+        console.log(visitsValues);
+        createVisitsGraph(visitsValues);
+    });
+}
+
+function createVisitsGraph(visitsValues){
+    var visits = new Chart("visits", {
+        type: "bar",
+        data:{
+            labels:months,
+            datasets: [{
+                data: visitsValues,
+                backgroundColor:"blue"
+            }]
+        },
+        options:{
+            responsive: true,
+            legend: {display: false},
+            title: {
+                display: true,
+                text: 'Nombre de stationnements par mois'
+            },
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        beginAtZero: true
+    
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function createExpensesGraph(expensesValues){
+    var expenses = new Chart("expenses", {
+        type: "line",
+        data:{
+            labels:months,
+            datasets: [{
+                borderColor: 'blue',
+                fill:false,
+                data: expensesValues,
+            }]
+        },
+        options:{
+            responsive: true,
+            legend: {display: false},
+            title: {
+                display: true,
+                text: 'Dépenses par mois'
+            },
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function sendQuery(url){
+    let result = [0,0,0,0,0,0,0,0,0,0,0,0];
+    // example : [{"d":"4","n":"12.00"}]
+    return fetch(url).then(function(response) {
+        if(response.status >= 200 && response.status < 300) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(function(response) {
+        if(response == "0"){
+            alert("Vous n'êtes pas connecté");
+        } else{
+            for(let i=0; i<response.length; i++){
+                result[parseInt(response[i]["d"])] = parseInt(response[i]["n"]);
+            }
+            return result;
+        }
+    });
+}
+
+function getExpensesValues(){
+    return sendQuery('./PHP/queryMsqli.php?d=expenses&y='+year);
+}
+
+function getVisitsValues(){
+    return sendQuery('./PHP/queryMsqli.php?d=visits&y='+year);
+}
+
+function refreshDate(){
+    let tempYear = document.getElementById('yearGraph').value;
+    let tempYearInt = parseInt(tempYear)
+    if(regex.test(tempYear) && tempYearInt >= 2000){
+        year = tempYear;
+
+        getExpensesValues().then((result) =>{
+            let expensesValues = result;
+            createExpensesGraph(expensesValues);
+        });
+        
+        getVisitsValues().then((result) =>{
+            let visitsValues = result;
+            createVisitsGraph(visitsValues);
+        });
+    } else{
+        document.getElementById('yearGraph').value = "2023";
+    }
+}
+
--- UserGraphs.php
+++ UserGraphs.php
@@ -7,9 +7,10 @@ session_start();
 <head>
 	<?php include_once("head.php"); ?>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+	<script src="UserGraphs.js"></script>
 </head>
 
-<body class="light">
+<body class="light" onload="initialize()">
 	<?php include_once("Header.php"); ?>
 	<div class="content">
 		<div class="horizontalMenu">
@@ -22,132 +23,6 @@ session_start();
 		</div>
 		<?php include_once("Footer.php"); ?>
 	</div>
-	<script>
-		let months=["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"];
-		let year = "2023";
-		let button = document.getElementById('refreshDate');
-		button.addEventListener("click",function(){refreshDate();});
-
-		function createVisitsGraph(visitsValues){
-			var visits = new Chart("visits", {
-				type: "bar",
-				data:{
-					labels:months,
-					datasets: [{
-						data: visitsValues,
-						backgroundColor:"blue"
-					}]
-				},
-				options:{
-					responsive: true,
-					legend: {display: false},
-					title: {
-						display: true,
-						text: 'Nombre de stationnements par mois'
-					},
-					scales: {
-						yAxes: [{
-							display: true,
-							ticks: {
-								beginAtZero: true
-			
-							}
-						}]
-					}
-				}
-			});
-		}
-
-		function createExpensesGraph(expensesValues){
-			var expenses = new Chart("expenses", {
-				type: "line",
-				data:{
-					labels:months,
-					datasets: [{
-						borderColor: 'blue',
-						fill:false,
-						data: expensesValues,
-					}]
-				},
-				options:{
-					responsive: true,
-					legend: {display: false},
-					title: {
-						display: true,
-						text: 'Dépenses par mois'
-					},
-					scales: {
-						yAxes: [{
-							display: true,
-							ticks: {
-								beginAtZero: true
-							}
-						}]
-					}
-				}
-			});
-		}
-
-		function sendQuery(url){
-			let result = [0,0,0,0,0,0,0,0,0,0,0,0];
-			// example : [{"d":"4","n":"12.00"}]
-			return fetch(url).then(function(response) {
-				if(response.status >= 200 && response.status < 300) {
-					return response.json();
-				}
-				throw new Error(response.statusText);
-			})
-			.then(function(response) {
-				if(response == "0"){
-					alert("Vous n'êtes pas connecté");
-				} else{
-					for(let i=0; i<response.length; i++){
-						result[parseInt(response[i]["d"])] = parseInt(response[i]["n"]);
-					}
-					return result;
-				}
-			});
-		}
-
-		function getExpensesValues(){
-			return sendQuery('./PHP/queryMsqli.php?d=expenses&y='+year);
-		}
-
-		function getVisitsValues(){
-			return sendQuery('./PHP/queryMsqli.php?d=visits&y='+year);
-		}
-
-		function refreshDate(){
-			year = document.getElementById('yearGraph').value;
-			getExpensesValues().then((result) =>{
-				let expensesValues = result;
-				createExpensesGraph(expensesValues);
-			});
-
-			getVisitsValues().then((result) =>{
-				let visitsValues = result;
-				createVisitsGraph(visitsValues);
-			});
-		}
-
-		getExpensesValues().then((result) =>{
-			let expensesValues = result;
-			createExpensesGraph(expensesValues);
-		});
-
-		getVisitsValues().then((result) =>{
-			let visitsValues = result;
-			console.log(visitsValues);
-			createVisitsGraph(visitsValues);
-		});
-
-
-		/*
-		To predict the next expenses:
-		- Use data of the previous year if there are any
-		- Use the previous months to get the slope expected
-		- Display it on the same graph ?
-		*/
-	</script>
+	
 </body>
 </html>
