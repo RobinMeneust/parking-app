c71299b88f7eee3b9a82d25fdfeab06c2f342c9f
Baat01 <75745165+Baat01@users.noreply.github.com>
2023-04-18 10:31:30 +0200

    V4
    Ajout envoi mail, renvoi sur la page form avec le message d'erreur ou de réussite, sauvegarde les info si mail impossible

--- PHP/formCheck.php
+++ PHP/formCheck.php
@@ -1,3 +1,16 @@
+
+
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Mail</title>
+    </head>
+
+    <body>
+
+</head>
 <?php
 
 $file =fopen("infoContact.json","a+") or die ("Fail");
@@ -5,86 +18,81 @@ $file =fopen("infoContact.json","a+") or die ("Fail");
 $Nom =$_GET['Nom'];
 $Prenom =$_GET['Prenom'];
 $datecontact =$_GET['datecontact'];
-$mail =$_GET['mail'];
+$email =$_GET['mail'];
 $gender =$_GET['gender'];
 $naissance =$_GET['naissance'];
 $typeMessage =$_GET['typeMessage'];
 $sujet =$_GET['sujet'];
 $contenu =$_GET['contenu'];
 $erreur="erreur=";
-
 //VERIFICATIONS VALIDITE ELEMENTS :
+
 if(!(gettype($Nom) == "string" && (strlen($Nom)>0 && strlen($Nom)<40))) {
     $erreur=$erreur . "Nom";
 }
 
 if(!(gettype($Prenom) == "string" && (strlen($Prenom)>0 && strlen($Prenom)<40))){
       if($erreur != "erreur="){
-        $erreur=$erreur . "_,_";
+        $erreur=$erreur . "+,+";
     }
     $erreur=$erreur . "Prenom";
 }
 
-if(!((gettype(explode("-",$datecontact,3)[0]) == "int" && gettype(explode("-",$datecontact,3)[1]) == "int" && gettype(explode("-",$datecontact,3)[2]) == "int") && checkdate(explode("-",$datecontact,3)[0],explode("-",$datecontact,3)[1],explode("-",$datecontact,3)[2]))){
+if(!(((string)(int) explode("-",$datecontact,3)[0] == explode("-",$datecontact,3)[0] && (string)(int) explode("-",$datecontact,3)[1] == explode("-",$datecontact,3)[1] && (string)(int) explode("-",$datecontact,3)[2] == explode("-",$datecontact,3)[2]) && checkdate(explode("-",$datecontact,3)[1],explode("-",$datecontact,3)[2],explode("-",$datecontact,3)[0]))){
     if($erreur != "erreur="){
-        $erreur=$erreur . "_,_";
+        $erreur=$erreur . "+,+";
     }
-    $erreur=$erreur . "Date_du_Message";
+    $erreur=$erreur . "Date+du+Message";
 }
 
-if(!(filter_var($mail, FILTER_VALIDATE_EMAIL) == true && (strlen($mail)>0 && strlen($mail)<40))){
+if(!(filter_var($email, FILTER_VALIDATE_EMAIL) == true && (strlen($email)>0 && strlen($email)<40))){
     if($erreur != "erreur="){
-        $erreur=$erreur . "_,_";
+        $erreur=$erreur . "+,+";
     }
     $erreur=$erreur . "Email";
 }
 
 if(!($gender == "male" || $gender == "female")){
     if($erreur != "erreur="){
-        $erreur=$erreur . "_,_";
+        $erreur=$erreur . "+,+";
     }
     $erreur=$erreur . "Genre";
 }
 
-if(!((gettype(explode("-",$naissance,3)[0]) == "int" && gettype(explode("-",$naissance,3)[1]) == "int" && gettype(explode("-",$naissance,3)[2]) == "int") && checkdate(explode("-",$naissance,3)[0],explode("-",$naissance,3)[1],explode("-",$naissance,3)[2]))){
+if(!(((string)(int) explode("-",$naissance,3)[0] == explode("-",$naissance,3)[0] && (string)(int) explode("-",$naissance,3)[1] == explode("-",$naissance,3)[1] && (string)(int) explode("-",$naissance,3)[2] == explode("-",$naissance,3)[2]) && checkdate(explode("-",$naissance,3)[1],explode("-",$naissance,3)[2],explode("-",$naissance,3)[0]))){
     if($erreur != "erreur="){
-        $erreur=$erreur . "_,_";
+        $erreur=$erreur . "+,+";
     }
-    $erreur=$erreur . "Date_de_Naissance";
+    $erreur=$erreur . "Date+de+Naissance";
 }
 
 if(!($typeMessage == "probleme" || $typeMessage == "question" || $typeMessage == "suggestion" || $typeMessage == "message")){
     if($erreur != "erreur="){
-        $erreur=$erreur . "_,_";
+        $erreur=$erreur . "+,+";
     }
-    $erreur=$erreur . "Type_du_Message";
+    $erreur=$erreur . "Type+du+Message";
 }
 
 if(!(gettype($sujet) == "string" && (strlen($sujet)>0 && strlen($sujet)<40))){
     if($erreur != "erreur="){
-        $erreur=$erreur . "_,_";
+        $erreur=$erreur . "+,+";
     }
     $erreur=$erreur . "Sujet";
 }
 
-if(!(gettype($contenu) == "string" && (strlen($contenu)>0 && strlen($contenu)<70))){
+if(!(gettype($contenu) == "string" && (strlen($contenu)>0 && strlen($contenu)<400))){
     if($erreur != "erreur="){
-        $erreur=$erreur . "_,_";
+        $erreur=$erreur . "+,+";
     }
-    $erreur=$erreur . "Contenu";
+    $erreur=$erreur . "Contenu+" . $contenu;
 }
 
 //RENVOI ERREUR / VALIDATION
 if($erreur != "erreur="){
-   header('location:Formulaire.html?' . $erreur);
-}
-else{
-    fwrite($file, $Nom.";".$Prenom.";".$datecontact.";".$mail.";".$gender.";".$naissance.";".$typeMessage.";".$sujet.";".$contenu."\r\n");
-    fclose($file);
-    echo "Merci pour votre confiance monsieur ".$Nom. "! Votre message est transmis à nos équipes qui vous contacterons dans les plus brefs délais." ;
+   header('location:../form.php?' . $erreur);
 }
 
-//MESSAGE RETENU
+/*//MESSAGE RETENU
 echo "<br><br>Info remplies dans le formulaire précédent :";
 echo "<br>Nom = ";
 echo $Nom;
@@ -93,7 +101,7 @@ echo $Prenom;
 echo "<br>Date de Contact = ";
 echo $datecontact;
 echo "<br>Adresse Mail = ";
-echo $mail;
+echo $email;
 echo "<br>Genre = ";
 echo $gender;
 echo "<br>Date de Naissance =";
@@ -103,7 +111,61 @@ echo $typeMessage;
 echo "<br>Sujet = ";
 echo $sujet;
 echo "<br>Contenu du Mail : <br>";
-echo $contenu;
+echo $contenu;*/
+
+// Import PHPMailer classes into the global namespace 
+use PHPMailer\PHPMailer\PHPMailer; 
+use PHPMailer\PHPMailer\SMTP; 
+use PHPMailer\PHPMailer\Exception; 
+ 
+// Include library files 
+require 'PHPMailer/Exception.php'; 
+require 'PHPMailer/PHPMailer.php'; 
+require 'PHPMailer/SMTP.php'; 
+ 
+// Create an instance; Pass `true` to enable exceptions 
+$mail = new PHPMailer; 
+ 
+// Server settings 
+//$mail->SMTPDebug = SMTP::DEBUG_SERVER;    //Enable verbose debug output 
+$mail->isSMTP();                            // Set mailer to use SMTP 
+$mail->Host = 'smtp.gmail.com';           // Specify main and backup SMTP servers 
+$mail->SMTPAuth = true;                     // Enable SMTP authentication 
+$mail->Username = 'baat01.p@gmail.com';       // SMTP username 
+$mail->Password = 'irrufbegunznzfjb';         // SMTP password 
+$mail->SMTPSecure = 'ssl';                  // Enable TLS encryption, `ssl` also accepted 
+$mail->Port = 465;                          // TCP port to connect to 
+ 
+// Sender info 
+$mail->setFrom('parkotop@app.com', 'ParkOTop'); 
+ 
+// Add a recipient 
+$mail->addAddress('baat01.p@gmail.com'); //adresse de reception
+ 
+//$mail->addCC('cc@example.com'); 
+//$mail->addBCC('bcc@example.com'); 
+ 
+// Set email format to HTML 
+$mail->isHTML(true); 
+ 
+// Mail subject 
+$mail->Subject = 'ParkOTop : ' . $typeMessage; 
+ 
+// Mail body content 
+$bodyContent = '<h1>'. $sujet . "</h1>"; 
+$bodyContent .= '<p>' . $contenu . '</p>'; 
+$bodyContent .=  '<br><p> Message envoyé par '. $Nom ." " . $Prenom ." (". $naissance.") le ".$datecontact.". Pour le contacter : ".$email."</p>";
+$mail->Body    = $bodyContent; 
+ 
+// Send email 
+if(!$mail->send()) { 
+    fwrite($file, $Nom.";".$Prenom.";".$datecontact.";".$email.";".$gender.";".$naissance.";".$typeMessage.";".$sujet.";".$contenu.";".$email->ErrorInfo."\r\n");
+    fclose($file);
+    header('location:../form.php?Message=' . "<br>Merci pour votre confiance monsieur ".$Nom. "! Votre message est enregistré et nos équipes vont l'examiner pour vous contacter dans les plus brefs délais. ");
+} else { 
+    header('location:../form.php?Message=' . "<br>Merci pour votre confiance monsieur ".$Nom. "! Votre message est transmis à nos équipes qui vous contacterons dans les plus brefs délais. " );
+}
+
 
 ?>
 
