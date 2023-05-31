ae0e90f46c1eb0c9e2eca71202e4521d92e01a9f
Romain Barré <rombar999@gmail.com>
2023-05-31 22:35:40 +0200

    change form style

--- form.php
+++ form.php
@@ -1,135 +1,123 @@
-<?php session_start(); 
+<?php session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <title>Formulaire</title>
-        <link rel="stylesheet" type="text/css" href="./assets/css/coucou.css">
-        <?php include_once("head.php"); ?>
-    </head>
 
-    <body>
-        <?php include_once("Header.php"); ?>
-
+<head>
+    <title>Formulaire</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/form.css">
+    <?php include_once("head.php"); ?>
 </head>
 
-<?php
-$erreur =$_GET['erreur']?? false;
-$success = $_GET['Message']?? false;
-echo'<div class=Overall>';
-if(gettype($erreur) == "string" && (strlen($erreur)>0))//if we get redirected here with an error, we print it
-{
-    echo'<div class="erreur">';
-    $pos = strrpos($erreur,',',0);
-    if(is_int($pos))
-    {
-        $fin= str_split($erreur, $pos-2);
-        $erreur=$fin[0].str_replace(',','et',$fin[1]);
-        echo "Veuiller remplir correctement les champs ".$erreur;
-    }
-    else{
-        echo "Veuiller remplir correctement le champ ".$erreur;
-    }
-    echo'</div>';
-}
-
-if(gettype($success) == "string" && (strlen($success)>0))//if the form was a success, we print it
-{
-    echo'<div class="success">';
-    echo $success;
-    echo'</div>';
-}
-
-
-?>
-
 <body>
-<div class="contact">
+    <?php include_once("Header.php"); ?>
 
- <form action="./PHP/formCheck.php" method="get"><!--Send it to formCheck-->
+    </head>
 
-    <div class="contactez-nous">
- <h1 style="text-align:center">Demande de contact</h1>
-<p>Un problème? Une question? Une suggestion? Ou juste envie de nous envoyer un message? N’hésitez pas à utiliser ce formulaire pour prendre contact avec l'équipe Park'o Top !</p>
+    <?php
+    $erreur = $_GET['erreur'] ?? false;
+    $success = $_GET['Message'] ?? false;
+    echo '<div class=Overall>';
+    if (gettype($erreur) == "string" && (strlen($erreur) > 0)) //if we get redirected here with an error, we print it
+    {
+        echo '<div class="erreur">';
+        $pos = strrpos($erreur, ',', 0);
+        if (is_int($pos)) {
+            $fin = str_split($erreur, $pos - 2);
+            $erreur = $fin[0] . str_replace(',', 'et', $fin[1]);
+            echo "Veuiller remplir correctement les champs " . $erreur;
+        } else {
+            echo "Veuiller remplir correctement le champ " . $erreur;
+        }
+        echo '</div>';
+    }
 
-   
-
-<table style="width: 70%;"--><!--form with all the information we need-->
-
- <tr>
-          <td>Date du message</td>
-          <td><input type="date" name="datecontact" placeholder="jj/mm/aaaa" required></td>
-      </tr>
-
- <tr>
-          <td>Nom </td>
-          <td><input type="text" name="Nom" size='40' placeholder="Entrez votre nom" required></td>
-      </tr>
-
- <tr>
-          <td>Prénom </td>
-          <td><input type="text" name="Prenom" size='40' placeholder="Entrez votre prénom" required></td>
-      </tr>
-
- <tr>
-          <td>Email </td>
-          <td><input type="email" name="mail" size='40' placeholder="monmail@messagerie.org" required></td>
-      </tr>
+    if (gettype($success) == "string" && (strlen($success) > 0)) //if the form was a success, we print it
+    {
+        echo '<div class="success">';
+        echo $success;
+        echo '</div>';
+    }
 
 
-<tr>
-<td>Genre </td>
-        <td style="width: 80%;">
-  <form>
-             <input type="radio" name="gender" value="female">Femme
-             <input type="radio" name="gender" value="male">Homme
-</form>
-    </td>
-    </tr>
+    ?>
 
+    <body>
+        <div class="container">
+      <div class="form">
+        <div class="contact-info">
+          <h3 class="title">Demande de contact</h3>
+          <p class="text">
+          Un problème ? Une question ? Une suggestion? Ou juste envie de nous envoyer un message? N'hésitez pas à utiliser ce formulaire pour prendre contact avec l'équipe Park'o Top !
+          </p>
+        </div>
 
+        <div class="contact-form">  
 
- <tr>
-          <td>Date de naissance</td>
-          <td><input type="date" name="naissance" placeholder="jj/mm/aaaa" required></td>
-      </tr>
+        <form action="./PHP/formCheck.php" method="get">
+            <h3 class="title">Contactez-nous</h3>
+            <div class="input-container">
+            <input class="input" type="text" name="datecontact"
+            onfocus="this.type='date'" onblur="if(!this.value) this.type='text'" required>
+            <label for="">Date du message</label>
+              <span>Date du message</span>
+            </div>
+            <div class="input-container">
+              <input class="input" type="text" name="nom" size='40' required>
+              <label for="">Nom</label>
+              <span>Nom</span>
+            </div>
+            <div class="input-container">
+              <input class="input" type="text" name="prenom" size='40' required>
+              <label for="">Prénom</label>
+              <span>Prénom</span>
+            </div>
+            <div class="input-container">
+            <input class="input" type="email" name="mail" size='40' required>
+              <label for="">Email</label>
+              <span>Email</span>
+            </div>
 
-      <tr>
-          <td>Type du message </td>
+            <div class="input-container">
+                <input type="radio" name="gender" value="female"><em class="select-gender">Femme</em>
+                <input type="radio" name="gender" value="male"><em class="select-gender">Homme</em>
+                <span style="color: #fff;">Genre</span>
+            </div>
+            
+            <div class="input-container">
+            <input class="input" type="text" name="naissance" required onfocus="this.type='date'" onblur="if(!this.value) this.type='text'">
+            <label for="">Date de naissance</label>
+              <span>Date de naissance</span>
+            </div>
+            
+            <div class="input-container">
+                <select name="typeMessage" class="select-type">
+                    <option value="probleme">Problème</option>
+                    <option value="question">Question</option>
+                    <option value="suggestion">Suggestion</option>
+                    <option value="message">Message Lambda</option>
+                </select>
+              <span style="color: #fff;">Type du message</span>
+            </div>
 
-          <td><select name="typeMessage" >
-             <option value="probleme">Problème</option>
-              <option value="question">Question</option>
-               <option value="suggestion">Suggestion</option>
-               <option value="message">Message Lambda</option>
-          </select></td>
+            <div class="input-container">
+            <input class="input" type="text" name="sujet" size='40' required>
+              <label for="">Sujet du message</label>
+              <span>Sujet du message</span>
+            </div>
 
-      </tr>
-
- <tr>
-          <td>Sujet: </td>
-          <td><input type="text" name="sujet" size='40' placeholder="Entrez le sujet de votre mail" required></td>
-      </tr>
-
-
- <tr>
-          <td>Contenu: </td>
-          <td><input type="text" name="contenu" size='70'style="height:110px;" placeholder="Tapez ici votre mail" required/></td>
-      </tr>
-
-
-    </table>
-    </fieldset>
-
-  <p style="margin-left: 500px;">
-          <input type="submit" value="Soumettre formulaire">
-            </input>
-    
-      </p>
-  </form>
-</div>
-</div>
-<?php include_once("Footer.php"); ?>
-  </body>
+            <div class="input-container textarea">
+              <textarea name="contenu" class="input"></textarea>
+              <label for="">Message</label>
+              <span>Message</span>
+            </div>
+            <input type="submit" value="Soumettre le formulaire" class="btn" />
+          </form>
+        </div>
+      </div>
+    </div>
+    <script src="./JS/form.js"></script>
+        <?php include_once("Footer.php"); ?>
+    </body>
 </html>
